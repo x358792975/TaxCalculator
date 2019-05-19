473a96f0903102232f48bcfc48aab97199257410
Sean Cui <sean900531@hotmail.com>
2019-05-19 19:26:03 -0400

    finished implementing

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,73 @@
-import javax.swing.*;
-import java.awt.*;
+import java.util.HashMap;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        String[] stateList = {"Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho"
-                              ,"Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan"};
-        // Minnesota Mississippi Missouri Montana Nebraska Nevada New Hampshire New Jersey
-        // New Mexico New York North Carolina North Dakota Ohio Oklahoma Oregon Pennsylvania
-        // Rhode Island South Carolina South Dakota Tennessee Texas Utah Vermont Virginia Washington West Virginia Wisconsin Wyoming
-        MyGUI gui = new MyGUI(stateList);
+        HashMap<String, Double> stateTax = new HashMap<>();
+        stateTax.put("Alabama",9.14);
+        stateTax.put("Alaska",1.43);
+        stateTax.put("Arizona",8.37);
+        stateTax.put("Arkansas",9.43);
+        stateTax.put("California",8.56);
+        stateTax.put("Colorado",7.63);
+        stateTax.put("Connecticut",6.35);
+        stateTax.put("Delaware",0.00);
+        stateTax.put("Florida",7.05);
+        stateTax.put("Georgia",7.29);
+        stateTax.put("Hawaii",4.41);
+        stateTax.put("Idaho",6.03);
+        stateTax.put("Illinois",8.74);
+        stateTax.put("Indiana",7.00);
+        stateTax.put("Iowa",6.82);
+        stateTax.put("Kansas",8.67);
+        stateTax.put("Kentucky",6.00);
+        stateTax.put("Louisiana",9.45);
+        stateTax.put("Maine",5.50);
+        stateTax.put("Maryland",6.00);
+        stateTax.put("Massachusetts",6.25);
+        stateTax.put("Michigan",6.00);
+        stateTax.put("Minnesota",7.43);
+        stateTax.put("Mississippi",7.07);
+        stateTax.put("Missouri",8.13);
+        stateTax.put("Montana",0.00);
+        stateTax.put("Nebraska",6.85);
+        stateTax.put("Nevada",8.14);
+        stateTax.put("New Hampshire",0.00);
+        stateTax.put("New Jersey",6.60);
+        stateTax.put("New Mexico",7.82);
+        stateTax.put("New York",8.49);
+        stateTax.put("North Carolina",6.97);
+        stateTax.put("North Dakota",6.85);
+        stateTax.put("Ohio",7.17);
+        stateTax.put("Oklahoma",8.92);
+        stateTax.put("Oregon",0.00);
+        stateTax.put("Pennsylvania",6.34);
+        stateTax.put("Rhode Island",7.00);
+        stateTax.put("South Carolina",7.43);
+        stateTax.put("South Dakota",6.40);
+        stateTax.put("Tennessee",9.47);
+        stateTax.put("Texas",8.19);
+        stateTax.put("Utah",6.94);
+        stateTax.put("Vermont",6.18);
+        stateTax.put("Virginia",5.65);
+        stateTax.put("Washington",9.17);
+        stateTax.put("West Virginia",6.39);
+        stateTax.put("Wisconsin",5.44);
+        stateTax.put("Wyoming",5.36);
+        stateTax.put("DC", 6.00);
+
+        HashMap<String, Double> currencyRate = new HashMap<>();
+        currencyRate.put("China", 6.92);
+        currencyRate.put("Korea",1194.74);
+        currencyRate.put("Canada", 1.34);
+        currencyRate.put("Australian", 1.45);
+        currencyRate.put("Japan", 110.10);
+        currencyRate.put("British", 0.79);
+
+        MyGUI gui = new MyGUI();
+        gui.initialUI(stateTax, currencyRate);
+
+
     }
 }
--- src/MyGUI.java
+++ src/MyGUI.java
@@ -1,36 +1,163 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.text.DecimalFormat;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 public class MyGUI extends JFrame {
 
-    String[] stateList;
-    JLabel label1;
-    JButton compute;
+    HashMap<String, Double> stateTax;
+    HashMap<String, Double> currencyRate;
+    JLabel label1, label2, label3, label4, label5;
+    JButton compute, btnCurrency;
+    JComboBox<String> statesBox, countryBox;
+    double amount,currencyResult, total;
+    JPanel panel;
+    JTextField txt;
+    String selectedState, selectedCountry;
+    TextArea taTax, taExchange;
+    DecimalFormat df = new DecimalFormat("#.00");
 
-    public MyGUI(String[] stateList){
+    public MyGUI(){
         super("Tax Calculator");
-        this.stateList = stateList;
+
+    }
+    public void initialUI(HashMap<String, Double> stateTax, HashMap<String, Double> currencyRate){
+
+
+        this.stateTax = stateTax;
+        this.currencyRate = currencyRate;
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(400, 400);
-        setVisible(true);
+        FlowLayout flowLayout = new FlowLayout(FlowLayout.LEFT);
+        setLayout(flowLayout);
+        flowLayout.setHgap(60);
+        flowLayout.setVgap(30);
+        panel = new JPanel();
+        JFrame f = new JFrame();
+        f.add(panel);
+        label1 = new JLabel();
 
-        Container con = this.getContentPane();
-        con.setLayout(null);
-        //setLayout(new FlowLayout());
-        label1 = new JLabel("Please select a state: ");
+        //label1.setForeground(Color.red);
+        label1.setText("Please Select a State:                   ");
+
+        panel.add(label1);
+
+        this.add(panel);
+        label2 = new JLabel();
+        label2.setText("Please enter the amount of money:           ");
+        this.add(label2);
+
+        label3 = new JLabel("Click button to compute :                              ");
+
+        ItemListener itemListener = new ItemListener() {
+            @Override
+            public void itemStateChanged(ItemEvent arg0) {
+                // TODO Auto-generated method stub
+                if(ItemEvent.SELECTED == arg0.getStateChange()){
+                    String selectedItem = arg0.getItem().toString();
+                    selectedState = selectedItem;
+                }
+                if(ItemEvent.DESELECTED == arg0.getStateChange()){
+                    String selectedItem = arg0.getItem().toString();
+                }
+            }
+        };
+
+
+        statesBox = new JComboBox<>();
+        statesBox.setEditable(true);
+        statesBox.addItemListener(itemListener);
+        Iterator iterator = stateTax.entrySet().iterator();
+        while(iterator.hasNext()){
+            Map.Entry current = (Map.Entry) iterator.next();
+            statesBox.addItem(current.getKey().toString());
+        }
+        statesBox.setEditable(false);
+        panel.add(statesBox);
+
+        add(label2);
+
+        txt = new JTextField(8);
+        taTax = new TextArea(2,50);
+
+
+        add(txt);
+        add(label3);
         compute = new JButton("Compute Tax");
-        //add(label1);
+        compute.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                amount = Integer.parseInt(txt.getText());
+                double taxRate = (stateTax.get(selectedState))/100;
+                total = (amount * (1+ taxRate ));
+                taTax.setText("Final Amount: " + df.format(total));
+                System.out.println("Amount :  " + amount);
+                System.out.println("total  " + total);
+            }
+        });
 
-        JComboBox myJComb = new JComboBox(stateList);
-        //JList jList = new JList(stateList);
-        //JScrollPane jsPane = new JScrollPane(jList);
 
-        JPanel jp1 = new JPanel();
-        jp1.add(myJComb);
-        con.add(label1);
-        //con.add(BorderLayout.WEST, jsPane);
-        //con.add(BorderLayout.EAST, jp1);
+        add(compute);
+        taTax.setEditable(false);
+        add(taTax);
 
-        jp1.setBounds(50, 50, 300, 200);
+        label4 = new JLabel("Please select a country:                       ");
+        add(label4);
+
+        ItemListener countryListener = new ItemListener() {
+            @Override
+            public void itemStateChanged(ItemEvent arg0) {
+                // TODO Auto-generated method stub
+                if(ItemEvent.SELECTED == arg0.getStateChange()){
+                    String selectedItem = arg0.getItem().toString();
+                    selectedCountry = selectedItem;
+                }
+                if(ItemEvent.DESELECTED == arg0.getStateChange()){
+                    String selectedItem = arg0.getItem().toString();
+                }
+            }
+        };
+
+        countryBox = new JComboBox<>();
+        countryBox.setEditable(true);
+        countryBox.addItemListener(countryListener);
+        Iterator iterator2 = currencyRate.entrySet().iterator();
+        while(iterator2.hasNext()){
+            Map.Entry current = (Map.Entry) iterator2.next();
+            countryBox.addItem(current.getKey().toString());
+        }
+        countryBox.setEditable(false);
+
+        add(countryBox);
+
+        label5 = new JLabel("Click button to compute foreign currency exchange: ");
+        add(label5);
+
+        btnCurrency = new JButton("Compute Currency Exchange: " );
+        btnCurrency.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                double curRate = currencyRate.get(selectedCountry);
+                System.out.println(curRate);
+                currencyResult = total * curRate;
+                taExchange.setText(df.format(currencyResult));
+            }
+        });
+        add(btnCurrency);
+
+        taExchange = new TextArea(2,50);
+        add(taExchange);
+
+        taExchange.setEditable(false);
+
+        this.setSize(500, 600);
+        this.setResizable(false);
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
     }
 }
